5e518b196cf50c209686835c263e77f2d87f0dc2
Andrew Beardsley <abeardsley@vertex.com>
2022-03-25 10:55:24 -0500

    Add search results.  Add style

--- src/search-component/index.js
+++ src/search-component/index.js
@@ -9,11 +9,14 @@ export default class Search extends React.Component {
         super(props);
 
         this.state = {
+            search: '',
             suggestions: [],
-            searchtimeout: null
+            searchtimeout: null,
+            results: {}
         }
 
         this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.doSearch = this.doSearch.bind(this);
     }
 
     handleSearchChange(event) {
@@ -23,16 +26,45 @@ export default class Search extends React.Component {
 
         if (!value || value.length <= 0)
         {
-            this.setState({ suggestions: [] })
+            this.setState({ 
+                search: '',
+                suggestions: [] 
+            })
+
             return;
         };
 
         this.setState({
+            search: value,
             //debounce easy mode - prevent sending request on every keystroke unless N milliseconds have gone by
             searchTimeout: setTimeout(() => {
                 this.getSuggestions(value);
-              }, 300)
+              }, 200)
         });
+    }
+
+    doSearch(value)
+    {
+        console.log("do search for: ", value);
+        this.setState({ 
+            search: value,
+            suggestions: [] });
+
+        let searchUrl= "https://everlastic.net/api/search/?includeKeymatches=true&page=1&pageSize=20&index=www.austinregionalclinic.com&highlightPre=%3Cstrong%3E&highlightPost=%3C/strong%3E&limitKeymatchesToStartsWith=true&q=" + value;
+        fetch(searchUrl)
+        .then(res => res.json())
+        .then(
+          (result) => {
+              console.log("Got results:", result);
+              this.setState({results: result});
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components. 
+          (error) => {
+              console.log("ERROR getting suggestions:", error);
+          }
+        )
     }
 
     getSuggestions(value)
@@ -56,25 +88,37 @@ export default class Search extends React.Component {
     }
 
     render() {
-        const { suggestions } = this.state;
+        const { suggestions, results } = this.state;
 
         return (
             <div className="Search">
-                <h2>Everlastic Search Suggestions</h2>
-                <p>
-                    Start typing a search to get search suggestions.
-                </p>
+                <h2>Everlastic Search - ARC</h2>
                 <input type="text" placeholder="Search..." value={this.state.search} onChange={this.handleSearchChange} />
                 {
                     (suggestions && suggestions.length > 0) &&
-                    <div>
+                    <div className="suggestionsContainer">
                         <ul>
                             {suggestions.map(suggestion => (
-                                <li key={suggestion}>
+                                <li key={suggestion} onClick={() => this.doSearch(suggestion)}>
                                     {suggestion}
                                 </li>
                             ))}
                         </ul>
+                    </div>
+                }
+
+                {           
+                    (results && results.Results && results.Results.length > 0) &&
+                    <div>
+                        {results.Results.map((result, index) => (
+                            <div className="searchResultContainer" key={ index }>
+                               <h4><a href={result._source.url}>{result._source.title}</a></h4>
+                               <div className="searchResultDetails">
+                                    <div>{result._source.url}</div>
+                                    <div dangerouslySetInnerHTML={{ __html: result.highlight }}></div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 }
             </div>
